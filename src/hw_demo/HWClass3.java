9354ca17c31b1eeb91a9748cb887371ed178f5dd
GYTumlisan <gymoa2011@gmail.com>
2023-04-05 21:42:50 -0700

    Initial commit repository3

---
+++ src/hw_demo/HWClass3.java
@@ -0,0 +1,12 @@
+package hw_demo;
+
+public class HWClass3 {
+
+
+	public static void main(String[] args) {
+		
+		System.out.println("Local repository 3....");
+		
+		System.out.println("Remote repository 3....");
+	}
+}
